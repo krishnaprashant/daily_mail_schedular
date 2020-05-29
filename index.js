e1e551179e3811101e06309b25802e784cb7a2a0
Harithsa <harithsa@thenexteam.com>
2020-05-29 13:32:06 +0530

    Indentation Fix

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 /*
-
 Author Prashant Krishna
-
 */
 
 var nodemailer = require('nodemailer');
 var urlListText = readTextFile();
 var urlList = urlListText.split('$$');
 
-
-function readTextFile(){
+function readTextFile() {
     const fs = require('fs');
-    try {        
+    try {
         const data = fs.readFileSync('text_498.txt', 'UTF-8');
         const lines = data.split(";");
-        var rawLines = "";        
+        var rawLines = "";
         lines.forEach((line) => {
-            rawLines += line;        
+            rawLines += line;
         });
-        return rawLines;        
+        return rawLines;
     } catch (err) {
         console.error(err);
     }
 }
 
 function modifyUrl(url) {
-    
-    
-    
     var param_1 = url.split('^');
-    
+
     var dates = param_1[1].split('_');
     var date_1 = dates[1];
     var date_2 = dates[2];
 
     url = url.replace(date_1, getPreviousDate());
-    url = url.replace(date_2, getNextDate());            
+    url = url.replace(date_2, getNextDate());
     return url;
 }
 
@@ -55,10 +49,8 @@ function getNextDate() {
     return ts;
 }
 
+function sendEmail(mail_subject, mail_email, mail_body) {
 
-
-function sendEmail(mail_subject,mail_email,mail_body){
-   
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -86,9 +78,9 @@ function sendEmail(mail_subject,mail_email,mail_body){
 urlList.forEach((item, index) => {
     var url_ = item.split("**")[0];
     url_ = modifyUrl(url_);
-    url_ = url_.replace(/\^/g, ";");    
+    url_ = url_.replace(/\^/g, ";");
     var subject_ = item.split("**")[1];
-    msg = "hi,\nyou can check your dashboard with this url below\n."+''+url_;
-    sendEmail(subject_,"prashantkrishna5@gmail.com",msg);
+    msg = "hi,\nyou can check your dashboard with this url below\n." + '' + url_;
+    sendEmail(subject_, "prashantkrishna5@gmail.com", msg);
 });
 
